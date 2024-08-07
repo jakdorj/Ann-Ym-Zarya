f5e2b6e96d3f3e6669e7f68d62ad4cdad6db8568
amarbayar <amarbayar.b@qpay.mn>
2024-08-07 11:15:23 +0800

    add fix

--- src/components/footer/FooterCopyright.js
+++ src/components/footer/FooterCopyright.js
@@ -1,17 +1,17 @@
 import PropTypes from "prop-types";
 import clsx from "clsx";
 import { Link } from "react-router-dom";
-import { useContext } from "react";
-import MainContext from "../mainContext/mainContext";
+// import { useContext } from "react";
+// import MainContext from "../mainContext/mainContext";
 
 const FooterCopyright = ({ footerLogo, spaceBottomClass, colorClass }) => {
-  const mainContext = useContext(MainContext);
+  // const mainContext = useContext(MainContext);
   return (
     <div className={clsx("copyright", spaceBottomClass, colorClass)}>
       <div className="footer-logo">
         <Link to={process.env.PUBLIC_URL + "/"}>
-          {/* <img alt="" src={process.env.PUBLIC_URL + footerLogo} /> */}
-          <img alt="" src={mainContext.logo.logoBlack} />
+          <img alt="" src={process.env.PUBLIC_URL + footerLogo} />
+          {/* <img alt="" src={mainContext.logo.logoBlack} /> */}
         </Link>
       </div>
       <p>
